36a0f14b5323e88057d99998503f1bc08923465b
Evgeny Evseev <evgeny@evseevs.ru>
2025-04-18 18:24:12 +0700

    tracing

--- config/config.go
+++ config/config.go
@@ -23,10 +23,21 @@ type YandexOAuthConfig struct {
 	ClientSecret string `yaml:"client_secret" env:"YANDEX_OAUTH_CLIENT_SECRET" env-default:"331b7a0292044e958989397a28c56bc7"`
 }
 
+type TelemetryConfig struct {
+	Tracing TracingConfig `yaml:"tracing"`
+}
+
+type TracingConfig struct {
+	Endpoint string `yaml:"endpoint" env:"OTEL_EXPORTER_OTLP_ENDPOINT" env-default:"otlp-gateway-prod-eu-west-2.grafana.net/otlp"`
+	Headers  string `yaml:"headers" env:"OTEL_EXPORTER_OTLP_HEADERS" env-default:"Authorization=Basic Nzg3MDQ0OmdsY19leUp2SWpvaU9UZzNPVGMzSWl3aWJpSTZJbk4wWVdOckxUYzROekEwTkMxdmRHeHdMWGR5YVhSbExXUmxkaUlzSW1zaU9pSkNVSFJGT0RjMGRXSkRiVEpNUlRGdWJVWTRORFUyTUZRaUxDSnRJanA3SW5JaU9pSndjbTlrTFdWMUxYZGxjM1F0TWlKOWZRPT0="`
+	Insecure bool   `yaml:"insecure" env:"OTEL_EXPORTER_OTLP_INSECURE" env-default:"false"`
+}
+
 type Config struct {
-	Server      ServerConfig
-	Database    DatabaseConfig
-	YandexOAuth YandexOAuthConfig
+	Server      ServerConfig      `yaml:"server"`
+	Database    DatabaseConfig    `yaml:"database"`
+	YandexOAuth YandexOAuthConfig `yaml:"yandex_oauth"`
+	Telemetry   TelemetryConfig   `yaml:"telemetry"`
 }
 
 func GetConfigOrDie() *Config {
