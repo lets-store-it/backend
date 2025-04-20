617a9b5d4b1f89eb354ea8002202693b593b4efb
Evgeny Evseev <evgeny@evseevs.ru>
2025-04-20 14:30:37 +0700

    sync

--- schema.sql
+++ schema.sql
@@ -65,7 +65,7 @@ CREATE TABLE item_variant (
     name VARCHAR(255) NOT NULL,
 
     article VARCHAR(255),
-    ean13 INTEGER CHECK (ean13 IS NULL OR ean13 ~ '^[0-9]{13}$'),
+    ean13 INTEGER,
 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP,
@@ -75,20 +75,6 @@ CREATE INDEX item_variant_item_id_idx ON item_variant(item_id);
 CREATE INDEX item_variant_ean13_idx ON item_variant(ean13);
 CREATE INDEX item_variant_article_idx ON item_variant(article);
 
-CREATE TABLE item_instance (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    org_id UUID NOT NULL REFERENCES org(id),
-    item_id UUID NOT NULL REFERENCES item(id),
-    variant_id UUID NOT NULL REFERENCES item_variant(id),
-
-    cell_id UUID REFERENCES cell(id),
-    status VARCHAR(255) NOT NULL CHECK (status IN ('available', 'reserved', 'consumed')),
-    affected_by_operation_id UUID REFERENCES operation(id),
-
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP,
-    UNIQUE (item_id, variant_id)
-);
 
 CREATE TABLE cells_group (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -124,6 +110,22 @@ CREATE TABLE cell (
 );
 CREATE INDEX cell_cells_group_id_idx ON cell(cells_group_id, id);
 
+
+CREATE TABLE item_instance (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    org_id UUID NOT NULL REFERENCES org(id),
+    item_id UUID NOT NULL REFERENCES item(id),
+    variant_id UUID NOT NULL REFERENCES item_variant(id),
+
+    cell_id UUID REFERENCES cell(id),
+    status VARCHAR(255) NOT NULL CHECK (status IN ('available', 'reserved', 'consumed')),
+    -- affected_by_operation_id UUID REFERENCES operation(id),
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+    UNIQUE (item_id, variant_id)
+);
+
 CREATE TABLE app_user (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -154,7 +156,7 @@ CREATE TABLE app_role (
     description VARCHAR(255)
 );
 
-INSERT INTO app_role (id, name, display_name) VALUES
+INSERT INTO app_role (id, name, display_name, description) VALUES
     (1, 'org_owner', 'Владелец', 'Имеет полный доступ к Организации, может назначать Управляющих организацией'),
     (2, 'org_admin', 'Управляющий', 'Имеет полный доступ к Организации, может назначать Менеджеров организации'),
     (3, 'org_manager', 'Менеджер', 'Имеет доступ к управлению объектами Организации, может назначать Сотрудников склада'),
@@ -193,7 +195,7 @@ CREATE TABLE app_object_changes (
     user_id UUID NOT NULL REFERENCES app_user(id),
     action VARCHAR(255) NOT NULL CHECK (action IN ('create', 'update', 'delete')),
     time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    target_object_type INTEGER NOT NULL REFERENCES app_object_type(id),
+    target_object_type INTEGER NOT NULL REFERENCES object_type(id),
     target_object_id UUID NOT NULL,
     prechange_state JSONB,
     postchange_state JSONB
@@ -208,35 +210,21 @@ CREATE TABLE app_api_token (
     revoked_at TIMESTAMP
 );
 
---     id INTEGER PRIMARY KEY,
---     group VARCHAR(100) NOT NULL,
---     name VARCHAR(100) NOT NULL,
---     UNIQUE (group, name)
--- );
--- CREATE INDEX object_type_id_idx ON object_type(id);
-
-
-
- 
--- CREATE TABLE app_role_permission (
---     id INTEGER PRIMARY KEY,
---     permission VARCHAR(255) NOT NULL,
---     UNIQUE (role_id, permission)
+-- CREATE TABLE operation (
+--     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     type VARCHAR(255) NOT NULL CHECK (type IN ('pick', 'movement')),
+--     assigned_to UUID REFERENCES employee(id),
+--     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 -- );
 
--- CREATE TABLE role_permission (
---     role_id UUID NOT NULL REFERENCES role(id),
---     permission VARCHAR(255) NOT NULL,
---     UNIQUE (role_id, permission)
+-- CREATE TABLE operation_item (
+--     operation_id UUID NOT NULL REFERENCES operation(id),
+--     item_instance_id UUID NOT NULL REFERENCES item_instance(id),
+--     status VARCHAR(255) NOT NULL CHECK (status IN ('pending', 'picked', 'done', 'returned')),
+--     origin_cell_id UUID REFERENCES cell(id),
+--     destination_cell_id UUID REFERENCES cell(id)
 -- );
 
-
--- INSERT INTO object_type (id, group, name) VALUES 
---     (1, 'storage', 'group'),
---     (2, 'storage', 'cells-group'),
---     (3, 'storage', 'cell'),
---     (4, 'items', 'item'),
---     (5, 'items', 'instance');
 
 -- CREATE TABLE custom_field (
 --     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -262,7 +250,6 @@ CREATE TABLE app_api_token (
 -- CREATE INDEX custom_field_related_types_custom_field_id_idx ON custom_field_related_types(custom_field_id);
 -- CREATE INDEX custom_field_related_types_object_type_id_idx ON custom_field_related_types(object_type_id);
 
-
 -- CREATE TABLE cell_kind (
 --     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 --     org_id UUID NOT NULL REFERENCES org(id),
@@ -274,115 +261,4 @@ CREATE TABLE app_api_token (
 --     UNIQUE (org_id, name)
 -- );
 
--- CREATE TABLE cell_group (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     space_id UUID NOT NULL REFERENCES storage_group(id),
---     name VARCHAR(255) NOT NULL,
---     short_name VARCHAR(255),
---     UNIQUE (space_id, name)
--- );
-
--- CREATE TABLE cell (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     short_name VARCHAR(255) NOT NULL,
---     group_id UUID NOT NULL REFERENCES cell_group(id),
---     kind_id UUID NOT NULL REFERENCES cell_kind(id),
---     rack VARCHAR(255) NOT NULL,
---     level INTEGER NOT NULL,
---     position INTEGER NOT NULL,
---     UNIQUE (group_id, rack, level, position)
--- );
-
--- CREATE TABLE item (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     org_id UUID NOT NULL REFERENCES org(id),
---     name VARCHAR(255) NOT NULL,
---     ean VARCHAR(255) NOT NULL,
---     UNIQUE (org_id, ean)
--- );
-
--- CREATE TABLE item_property (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     item_id UUID NOT NULL REFERENCES item(id),
---     name VARCHAR(255) NOT NULL,
---     type VARCHAR(255) NOT NULL CHECK (type IN ('text', 'bool', 'number')),
---     value VARCHAR(255) NOT NULL,
---     UNIQUE (item_id, name)
--- );
-
--- CREATE TABLE item_variant (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     item_id UUID NOT NULL REFERENCES item(id),
---     name VARCHAR(255) NOT NULL,
---     width INTEGER,
---     depth INTEGER,
---     height INTEGER,
---     weight INTEGER,
---     UNIQUE (item_id, name),
---     CHECK ((width IS NULL AND depth IS NULL AND height IS NULL AND weight IS NULL) OR 
---            (width IS NOT NULL AND depth IS NOT NULL AND height IS NOT NULL AND weight IS NOT NULL))
--- );
-
--- CREATE TABLE item_variant_property (
---     variant_id UUID NOT NULL REFERENCES item_variant(id),
---     property_id UUID NOT NULL REFERENCES item_property(id),
---     value VARCHAR(255) NOT NULL,
---     UNIQUE (variant_id, property_id)
--- );
-
--- CREATE TABLE item_instance (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     tracking_id VARCHAR(255) NOT NULL UNIQUE,
---     item_id UUID NOT NULL REFERENCES item(id),
---     variant_id UUID NOT NULL REFERENCES item_variant(id),
---     cell_id UUID REFERENCES cell(id),
---     status VARCHAR(255) NOT NULL CHECK (status IN ('available', 'reserved', 'consumed')),
---     affected_by_operation_id UUID REFERENCES operation(id)
--- );
-
--- CREATE TABLE app_user (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     fullname VARCHAR(255),
---     email VARCHAR(255) NOT NULL UNIQUE
--- );
-
--- CREATE TABLE employee (
---     user_id UUID NOT NULL REFERENCES app_user(id),
---     org_id UUID NOT NULL REFERENCES org(id),
---     PRIMARY KEY (user_id, org_id)
--- );
-
--- CREATE TABLE role (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     name VARCHAR(255) NOT NULL,
---     display_name VARCHAR(255) NOT NULL
--- );
-
--- CREATE TABLE role_permission (
---     role_id UUID NOT NULL REFERENCES role(id),
---     permission VARCHAR(255) NOT NULL,
---     UNIQUE (role_id, permission)
--- );
-
--- CREATE TABLE role_binding (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     role_id UUID NOT NULL REFERENCES role(id),
---     employee_id UUID NOT NULL REFERENCES employee(id),
---     UNIQUE (role_id, employee_id)
--- );
-
--- CREATE TABLE operation (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     type VARCHAR(255) NOT NULL CHECK (type IN ('pick', 'movement')),
---     assigned_to UUID REFERENCES employee(id),
---     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
--- );
-
--- CREATE TABLE operation_item (
---     operation_id UUID NOT NULL REFERENCES operation(id),
---     item_instance_id UUID NOT NULL REFERENCES item_instance(id),
---     status VARCHAR(255) NOT NULL CHECK (status IN ('pending', 'picked', 'done', 'returned')),
---     origin_cell_id UUID REFERENCES cell(id),
---     destination_cell_id UUID REFERENCES cell(id)
--- );
 
--- test-data.sql
+++ test-data.sql
@@ -1,28 +1,38 @@
 INSERT INTO org (id, name, subdomain) VALUES
-    ('a4da775e-dbd4-4e1c-a254-232a4686dcde', 'ООО "Экзотика"', 'exotic-inc'),
     ('453f0e17-c8f4-4c99-9d20-f0e13572550e', 'ООО "TapMe"', 'tapme');
 
-INSERT INTO org_unit (id, org_id, name, alias) VALUES
-    ('ba548db7-ceae-4ff6-bc30-653f45a53ddd', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Москва Амурская', 'AM'),
-    ('c3ac04a4-043d-4b34-b2bd-4b7e138ca3ad', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Красноярск 1', 'KR1'),
+INSERT INTO org_unit (id, org_id, name, alias, address) VALUES
+    ('ba548db7-ceae-4ff6-bc30-653f45a53ddd', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Москва Амурская', 'AM', 'ул. Амурская, 1'),
+    ('c3ac04a4-043d-4b34-b2bd-4b7e138ca3ad', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Красноярск 1', 'KR1', NULL);
 
-    ('3b6e1ac8-66c2-4315-94f5-f6276eaefed9', 'a4da775e-dbd4-4e1c-a254-232a4686dcde', 'ГимН3', 'MOF');
+INSERT INTO storage_group (id, org_id, unit_id, parent_id, name, alias) VALUES
+    ('73717d25-ba60-459a-bf8e-736ba9fa3e60', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'ba548db7-ceae-4ff6-bc30-653f45a53ddd', NULL, 'Основная группа', 'AOG'),
+    ('3fa3f6a0-5c9b-4360-bf2d-110a7acd6745', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'c3ac04a4-043d-4b34-b2bd-4b7e138ca3ad', NULL, 'Первый Этаж', 'KFS'),
+    ('3726e287-b6a5-47b3-b178-11f99f860fe3', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'c3ac04a4-043d-4b34-b2bd-4b7e138ca3ad', '3fa3f6a0-5c9b-4360-bf2d-110a7acd6745', 'Зона А', 'KZA');
 
-brbr    ('73717d25-ba60-459a-bf8e-736ba9fa3e60', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'ba548db7-ceae-4ff6-bc30-653f45a53ddd', NULL, 'Входная зона', 'EN1'),
-    ('3fa3f6a0-5c9b-4360-bf2d-110a7acd6745', 'a4da775e-dbd4-4e1c-a254-232a4686dcde', '3b6e1ac8-66c2-4315-94f5-f6276eaefed9', NULL, 'Основная группа', 'PS'),
-    ('3726e287-b6a5-47b3-b178-11f99f860fe3', 'a4da775e-dbd4-4e1c-a254-232a4686dcde', '3b6e1ac8-66c2-4315-94f5-f6276eaefed9', '3fa3f6a0-5c9b-4360-bf2d-110a7acd6745', 'Зона А', 'ZA');
+INSERT INTO cells_group (id, org_id, storage_group_id, name, alias) VALUES
+    ('c58851e0-0233-4d20-b965-48992ef8f9fc', '453f0e17-c8f4-4c99-9d20-f0e13572550e', '3726e287-b6a5-47b3-b178-11f99f860fe3', 'Стеллаж 1', 'SFJ1');
 
-INSERT INTO item (id, org_id, name, description) VALUES
-    ('5b29eb11-c699-414b-a351-32476ad9dae0', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Визитка TapMe Карта', 'Визитка в виде карты'),
-    ('5985325b-2e81-4d4a-8081-b96f034c81bb', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Визитка TapMe Стикер', 'Визитка в виде стикера'),
+INSERT INTO cell (id, org_id, cells_group_id, alias, row, level, position) VALUES
+    ('65f7de59-0848-48b2-b541-1a072d7ae461', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'c58851e0-0233-4d20-b965-48992ef8f9fc', 'A11', 1, 1, 1),
+    ('8ee7ee39-8471-4537-98d8-0da9ae25043e', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'c58851e0-0233-4d20-b965-48992ef8f9fc', 'A12', 1, 2, 1),
+    ('77dea1ec-6ce9-45f5-abae-3dc27fc23b82', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'c58851e0-0233-4d20-b965-48992ef8f9fc', 'A13', 1, 3, 1);
 
-    ('97e4d2cf-8519-4cf9-aff3-ca8368506fd2', 'a4da775e-dbd4-4e1c-a254-232a4686dcde', 'Давитель для корма', 'Давитель для корма');
+INSERT INTO item (id, org_id, name, description, width, depth, height, weight) VALUES
+    ('5b29eb11-c699-414b-a351-32476ad9dae0', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Визитка TapMe Карта', 'Визитка в виде карты', 100, 100, 100, 100),
+    ('5985325b-2e81-4d4a-8081-b96f034c81bb', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'Визитка TapMe Стикер', 'Визитка в виде стикера', 100, 100, 100, 100);
 
-INSERT INTO item_variant (id, item_id, name, article, ean13) VALUES
-    ('29ad8e0e-f187-47c7-a158-01de64a2aad3', '97e4d2cf-8519-4cf9-aff3-ca8368506fd2', 'Черная', 'TM01', NULL),
-    ('7513a498-9e6f-48fc-9787-e6395df65a85', '97e4d2cf-8519-4cf9-aff3-ca8368506fd2', 'Белая', 'TM02', NULL),
+INSERT INTO item_variant (id, item_id, org_id, name, article, ean13) VALUES
+    ('29ad8e0e-f187-47c7-a158-01de64a2aad3', '5985325b-2e81-4d4a-8081-b96f034c81bb', '453f0e17-c8f4-4c99-9d20-f0e13572550e','Черная', 'TM01', NULL),
+    ('7513a498-9e6f-48fc-9787-e6395df65a85', '5985325b-2e81-4d4a-8081-b96f034c81bb', '453f0e17-c8f4-4c99-9d20-f0e13572550e','Белая', 'TM02', NULL),
 
-    ('99921684-433c-4ebe-a461-d5a9d19519b3', '97e4d2cf-8519-4cf9-aff3-ca8368506fd2', 'Основной', NULL, NULL)
+    ('99921684-433c-4ebe-a461-d5a9d19519b3', '5b29eb11-c699-414b-a351-32476ad9dae0', '453f0e17-c8f4-4c99-9d20-f0e13572550e','Основной', NULL, NULL)
 
 INSERT INTO app_user (id, email, first_name, last_name, middle_name) VALUES
-    ('99921684-433c-4ebe-a461-d5a9d19519b3', 'evgeny@evseevs.ru', 'Евгений', 'Евсеев', 'Васильевич');
+    ('71254334-e6d0-47bb-8483-d13cc8456910', 'evseev-2003@yandex.ru', 'Евгений', 'Евсеев', 'Васильевич');
+
+INSERT INTO app_role_binding (id, org_id, role_id, user_id) VALUES
+    ('094ae0c3-23fb-4c0d-bd36-283f040eb8a3', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 1, '71254334-e6d0-47bb-8483-d13cc8456910');
+
+INSERT INTO app_api_token (id, org_id, name, token) VALUES
+    ('b826bb73-6018-4342-a122-66f418442ba2', '453f0e17-c8f4-4c99-9d20-f0e13572550e', 'testin-token', 'super-secret-token');
